1283a8a5acfda89b0c538265cc781c3f47272a80
蓝剑桥 <lanjianqiao@lanjianqiaodeMacBook-Pro.local>
2023-03-13 20:51:48 +0800

    init: 初始化项目

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -2,6 +2,6 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'header-max-length': [0, 'always', 72],
-    'type-enum': ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert', 'init'],
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert', 'init']],
   },
 }
